347bee26a9f13110cc2edc080d6a9907a81b4ee0
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-06-28 06:21:26 +0700

    ready for lotto interface

---
+++ lotto/index.js
@@ -0,0 +1,22 @@
+
+class DappInterface {
+    constructor() {
+        this.selectedDropdown1 = ''; // set via the HTML input; captures the chosen token exchange
+        this.JSDropdownInput1 = document.getElementById('HTML_dropdown_input_1'); // dropdown to choose tokens
+    }
+
+    async chooseWhichLotto(){
+            this.selectedDropdown1 = this.JSDropdownInput1.value;
+            console.log(this.selectedDropdown1);
+
+            if(this.selectedDropdown1 == "MATIC"){
+            window.location.href = "./MATIC.html";
+            }
+            /*
+            if(this.selectedDropdown1 == "BANK2BON"){
+            window.location.href = "./bank2bon.html";
+            } */
+    }
+}
+
+const DappInterface_ = new DappInterface();
