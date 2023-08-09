b833908aaee8dde682da098a357a77e6402e7335
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-08-09 14:21:56 +0700

    need to update the listeners now!

--- stake/indexInterface.js
+++ stake/indexInterface.js
@@ -46,7 +46,7 @@ class IndexInterface {
         console.log(this.JSInputValue1);
 
         if(this.JSInputValue1 == "BONNFT"){
-            window.location.href = "./nfts/BonnftStake.html";
+            window.location.href = "./BonnftStake.html";
         }
     }
 }
